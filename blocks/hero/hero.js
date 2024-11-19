ea06a1e04ee1d5c5d7fef702f3795a0908f7b27f
Kam <karadhak@adobe.com>
2024-11-19 19:33:15 +1100

    update

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -12,17 +12,3 @@ export default async function decorate(block) {
     btnContainer.querySelector('a').classList.add('button-primary');
   }
 }
-/**
- * loads and decorates the hero
- * @param {Element} block The hero block element
- */
-
-export default async function decorate(block) {
-  // Target the third child div to add button-primary class
-  const div = block.children[2];
-
-  if (div) {
-    const btnContainer = div.querySelector('.button-container');
-    btnContainer.querySelector('a').classList.add('button-primary');
-  }
-}
